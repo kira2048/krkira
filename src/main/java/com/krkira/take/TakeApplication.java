7577a68b2d4348cf50cbb73ce7fbce346a528483
kirakira <zhuneihui@gmail.com>
2024-12-06 05:02:50 +0900

    Initial commit

---
+++ src/main/java/com/krkira/take/TakeApplication.java
@@ -0,0 +1,21 @@
+package com.krkira.take;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(exclude = { org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class })
+@RestController
+public class TakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
---
+++ src/test/java/com/krkira/take/TakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krkira.take;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
